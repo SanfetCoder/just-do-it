2689aad327718aa47a0a93c7dbb29918cfc999ed
SanphetLovesTina <sanphetlovestina@SanphetLovesTinas-MacBook-Air.local>
2023-10-13 20:41:38 +0700

    Init

---
+++ justapaper/src/HomePage.jsx
@@ -0,0 +1,72 @@
+import styles from "./css/Homepage.module.css";
+
+const Homepage = ({ onClose }) => {
+  return (
+    <div className={styles.homepage}>
+      <div className={styles.frame}>
+        <img className={styles.icon} alt="" src="/1695210983524@2x.png" />
+        <div className={styles.collaborations}>COLLABORATIONS</div>
+        <div className={styles.communities}>COMMUNITIES</div>
+        <div className={styles.marketplace}>Marketplace</div>
+      </div>
+      <div className={styles.imageRemovebgPreview121Parent}>
+        <img
+          className={styles.imageRemovebgPreview121}
+          alt=""
+          src="/imageremovebgpreview-12-1@2x.png"
+        />
+        <img className={styles.face1Icon} alt="" src="/face-1@2x.png" />
+        <img className={styles.eye1Icon} alt="" src="/eye-1@2x.png" />
+        <img className={styles.base1Icon} alt="" src="/base-1@2x.png" />
+        <img className={styles.shirtPafe1} alt="" src="/shirt-pafe-1@2x.png" />
+      </div>
+      <img
+        className={styles.copyOfJustapaperPitch11}
+        alt=""
+        src="/copy-of-justapaper--pitch-11-2@2x.png"
+      />
+      <img
+        className={styles.copyOfJustapaperPitch3}
+        alt=""
+        src="/copy-of-justapaper--pitch-3-1@2x.png"
+      />
+      <button className={styles.hair1} />
+      <img
+        className={styles.copyOfJustapaperPitch10}
+        alt=""
+        src="/copy-of-justapaper--pitch-10-1@2x.png"
+      />
+      <img
+        className={styles.copyOfJustapaperPitch9}
+        alt=""
+        src="/copy-of-justapaper--pitch-9-1@2x.png"
+      />
+      <img
+        className={styles.copyOfJustapaperPitch111}
+        alt=""
+        src="/copy-of-justapaper--pitch-11-1@2x.png"
+      />
+      <button className={styles.copyOfJustapaperPitch8} />
+      <button className={styles.copyOfJustapaperPitch14} />
+      <button className={styles.copyOfJustapaperPitch15} />
+      <div className={styles.copyOfJustapaperPitch16} />
+      <button className={styles.copyOfJustapaperPitch13} />
+      <div className={styles.copyOfJustapaperPitch12Wrapper}>
+        <img
+          className={styles.copyOfJustapaperPitch12}
+          alt=""
+          src="/copy-of-justapaper--pitch-12-2@2x.png"
+        />
+      </div>
+      <button className={styles.imageRemovebgPreview221} />
+      <img
+        className={styles.copyOfJustapaperPitch112}
+        alt=""
+        src="/copy-of-justapaper--pitch-11-5@2x.png"
+      />
+      <button className={styles.copyOfJustapaperPitch121} />
+    </div>
+  );
+};
+
+export default Homepage;
